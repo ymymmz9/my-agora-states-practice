81733b00bada55f5964849644c0138ada8d1a68c
ymymmz9 <ymymmz9@gmail.com>
2022-12-07 15:04:43 +0900

    practice에 1번째 커

--- my-agora-states-client/src/components/Discussions.js
+++ my-agora-states-client/src/components/Discussions.js
@@ -2,12 +2,16 @@ import { Discussion } from "./Discussion";
 
 export const Discussions = ({ discussions, deleteDiscussion }) => {
   return (
+    
+    
     <section className="discussion__wrapper">
+      <div>
       <ul className="discussions__container">
         {discussions.map((discussion) => {
           return <Discussion key={discussion.id} discussion={discussion} deleteDiscussion={deleteDiscussion}/>
         })}
       </ul>
+      </div>
     </section>
   );
 };
